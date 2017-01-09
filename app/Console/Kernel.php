b14496e808a5473853f6f790418d8e54e7f74e0f
Mohamed Shapan <m7shapan@gmail.com>
2017-01-09 15:30:12 +0200

    typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('monitor:check-uptime')->everyMinute();
+        // $schedule->command('monitor:check-uptime')->everyMinute();
         $schedule->command('check:websites')->everyMinute();
     }
 
